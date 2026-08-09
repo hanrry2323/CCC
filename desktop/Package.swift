f8f23690b3e4262db5c9112c15e7ffefa0f8d8a3
CCC Dev <ccc-dev@localhost>
2026-08-10 00:04:17 +0800

    feat(desktop): ccc034 Package.swift 引入 SwiftUIX/swiftui-introspect/textual (ccc-plan-012)

--- desktop/Package.swift
+++ desktop/Package.swift
@@ -7,9 +7,19 @@ let package = Package(
     products: [
         .executable(name: "CCCDesktop", targets: ["CCCDesktop"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SwiftUIX/SwiftUIX.git", branch: "master"),
+        .package(url: "https://github.com/siteline/swiftui-introspect.git", from: "1.3.0"),
+        .package(url: "https://github.com/gonzalezreal/textual.git", from: "0.1.0"),
+    ],
     targets: [
         .executableTarget(
             name: "CCCDesktop",
+            dependencies: [
+                .product(name: "SwiftUIX", package: "SwiftUIX"),
+                .product(name: "SwiftUIIntrospect", package: "swiftui-introspect"),
+                .product(name: "Textual", package: "textual")
+            ],
             path: "Sources/CCCDesktop"
         ),
         .testTarget(
